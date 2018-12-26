ad6e07883c24a4997e4dcefad4a9bcd949980671
prokid106 <league@WTS-IM-6.attlocal.net>
2018-12-26 13:23:07 -0800

    day1

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,18 +6,18 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+Robot R = new Robot();
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+R.setSpeed(200);
 		// 5. Set the pen width to 5
-
+R.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
 
 			// 7. Set the pen color to random
 	
 			// 1. Call the drawSquare() method
-	
+	drawSquare();
 			// 8. Turn the robot 90 degrees to the right
 
 	}
@@ -26,7 +26,11 @@ public class FourSquare {
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
+		for (int i = 0; i<4; i++) {
+			R.penDown();
+			R.move(100);
+			R.turn(90);
+		}
 		
 	}
 
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,27 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
+	Robot boredbot = new Robot();
+	boredbot.setPenColor(Color.CYAN);
+	boredbot.penDown();
+	boredbot.setSpeed(200);
+	for (int i = 0; i <4; i++) {
+	boredbot.setRandomPenColor();
+	boredbot.setPenWidth(5);
+	boredbot.move(200);
+	boredbot.turn(90);
+	}
+	boredbot.move(100);
+	boredbot.turn(90);
+	boredbot.move(200);
+	boredbot.turn(-90);
+	boredbot.move(100);
+	boredbot.turn(-90);
+	boredbot.move(100);
+	boredbot.turn(-90);
+	boredbot.move(200);
+	boredbot.penUp();
+	boredbot.move(200);
+	boredbot.turn(180);
 	}
 }
